b8e003ec097babc1e5525016c26a4bab3e8318e0
BryanGonCa <bryangonca@gmail.com>
2024-02-19 12:27:26 -0400

    Casi terminado

---
+++ PeliculasWeb/Repositorio/CategoriaRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using PeliculasWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PeliculasWeb.Repositorio.IRepositorio
+{
+    public class CategoriaRepositorio : Repositorio<Categoria>, ICategoriaRepositorio
+    {
+        // Injeccion de dependencias se debe importar el IHttpClientFactory
+        private readonly IHttpClientFactory _clientFactory;
+
+        public CategoriaRepositorio(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        
+
+    }
+}
---
+++ PeliculasWeb/Repositorio/PeliculaRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using PeliculasWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PeliculasWeb.Repositorio.IRepositorio
+{
+    public class PeliculaRepositorio : Repositorio<Pelicula>, IPeliculaRepositorio
+    {
+        // Injeccion de dependencias se debe importar el IHttpClientFactory
+        private readonly IHttpClientFactory _clientFactory;
+
+        public PeliculaRepositorio(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        
+
+    }
+}
